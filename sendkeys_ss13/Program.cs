8ff4a050f8ff53a6c04c8d3df809e1f79face222
VistaPOWA <hegyi.mokka@gmail.com>
2014-04-13 18:04:18 +0200

    FINALLY IT'S DONE

--- sendkeys_ss13/Program.cs
+++ sendkeys_ss13/Program.cs
@@ -12,8 +12,11 @@ namespace sendkeys_ss13
 {
     public class Program
     {
+        //default values
         public static string serverIP = "127.0.0.1";
-        public static int serverPort = 7281;
+        public static int serverPort = 11111;
+        public static string commskey = "this_is_a_test_key";
+        public static string IRC_bot_name = "testbot_github";
 
         public static IrcClient irc = new IrcClient();
         public static void Main(string[]args) 
@@ -32,67 +35,105 @@ namespace sendkeys_ss13
 
         public static void ReadConf(ref string IP, ref int port)
         {
-            if (File.Exists("config.ini"))
+            if (File.Exists("config.txt"))
             {
-                StreamReader reader = new StreamReader("config.ini");
-                while (!reader.EndOfStream)
+                StreamReader reader = new StreamReader("config.txt");
+                string[] line = reader.ReadLine().Split(' ');
+                if (line[2] != null)
                 {
-                    string[] line = reader.ReadLine().Split(' ');
-                    Match match1 = Regex.Match(line[3], @"(\d{1,3}.?){4}"); //rudimentary IP validation
+                    Match match1 = Regex.Match(line[2], @"(\d{1,3}.?){4}"); //rudimentary IP validation
                     if (match1.Success)
                     {
-                        serverIP = line[3];
+                        serverIP = line[2];
+                        Console.WriteLine("Read IP: " + serverIP);
                     }
                     else { Console.WriteLine("IP cannot be validated."); return; }
-                    line = reader.ReadLine().Split(' ');
-                    Match match2 = Regex.Match(line[3], @"(\d{1,5}"); //rudimentary port validation
-                    if(match1.Success && (Convert.ToInt32(line[3]) < 65535))
+                }
+                line = reader.ReadLine().Split(' ');
+                if (line[2] != null)
+                {
+                    Match match2 = Regex.Match(line[2], @"\d{1,5}"); //rudimentary port validation
+                    if (match2.Success && (Convert.ToInt32(line[2]) < 65535))
                     {
-                        serverPort = Convert.ToInt32(line[3]);
+                        serverPort = Convert.ToInt32(line[2]);
+                        Console.WriteLine("Read port: " + serverPort);
                     }
                     else { Console.WriteLine("Port cannot be validated."); return; }
                 }
+                line = reader.ReadLine().Split(' ');
+                if (line[2] != null)
+                {
+                    commskey = line[2];
+                    Console.WriteLine("Commskey read.");
+                }
+                else { Console.WriteLine("No Commskey!"); return; }
+                line = reader.ReadLine().Split(' ');
+                if(line[2] != null)
+                {
+                    IRC_bot_name = line[2];
+                    Console.WriteLine("Read IRC bot name: " + IRC_bot_name);
+                }
+                else { Console.WriteLine("No botname found."); return; }
+                reader.Close();
             }
             else { Console.WriteLine("Config file doesn't exist, using defaults"); return; }
         }
         public static void OnChannelMessage(object sender, IrcEventArgs e) 
         {
-            if(e.Data.Nick == "vistapowa_tp")
+            if(e.Data.Nick == IRC_bot_name)
             {
+                StreamWriter output = new StreamWriter("output.txt");
                 string[] msg = e.Data.Message.Split(' ');
                 for (int i = 0; i < msg.Length; i++)
                 {
                     msg[i] = Regex.Replace(msg[i], @"[\x02\x1F\x0F\x16]|\x03(\d\d?(,\d\d?)?)?", String.Empty); //Sanitizing color codes
+                    msg[i] = Regex.Replace(msg[i], @"[\\\&\=\;]", " "); //Filtering out some iffy characters
+                    Console.Write(msg[i] + " ");
                 }
-                byte[] PACKETS = CreatePacket(msg);
-                TcpClient client = new TcpClient(serverIP, serverPort);
-                NetworkStream stream = client.GetStream();
-                stream.Write(PACKETS, 0, PACKETS.Length);
-                stream.Close();
-                client.Close();
+                Console.WriteLine();
+                if (msg[2] == "opened")
+                {
+                    byte[] PACKETS = CreatePacket(msg);
+                    PACKETS[1] = 0x83;
+                    int len = 0;
+                    for (int i = 0; i < msg.Length; i++)
+                    {
+                        len += msg[i].Length + 1;
+                    }
+                    len += 14 + commskey.Length + 6;
+                    PACKETS[3] = (byte) len;
+                                
+                    StringBuilder test = new StringBuilder();
+                    for (int i = 0; i < PACKETS.Length; i++)
+                    {
+                        test.Append(Convert.ToString(PACKETS[i]));
+                    }
+                    output.WriteLine(Convert.ToString(test));
+                    Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    IPEndPoint ip = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+
+                    server.Connect(ip);
+                    server.Send(PACKETS);
+                    output.Close();
+                }
             }
         }
 
         private static byte[] CreatePacket(string[] msg)
         {
             StringBuilder packet = new StringBuilder();
-            packet.Append('\x00');
-            packet.Append('\x87');
-            int len = 0;
+            packet.Append((char)'\x00');
+            byte x83 = 0x83 ;
+            packet.Append((char)x83);
+            packet.Append((char)'\x00',6);
+            packet.Append("?announce=");
             for (int i = 0; i < msg.Length; i++)
             {
-                len += msg[i].Length;
+                packet.Append(msg[i] + " ");
             }
-            packet.Append((char)(len + 6));
-            for (int i = 0; i < 6; i++)
-            {
-                packet.Append('\x00');
-            }
-            for (int i = 0; i < msg.Length; i++)
-            {
-                packet.Append(msg[i]);
-            }
-            packet.Append('\x00');
+            packet.Append("&key=");
+            packet.Append(commskey);
+            packet.Append((char)'\x00');
             return Encoding.ASCII.GetBytes(packet.ToString()); 
         } 
     }
